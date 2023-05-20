6daf696081716c1ced6991d164dfb2b502998a88
Abdurehman Saleemi <76134547+AbdurehmanSaleemi@users.noreply.github.com>
2023-05-20 18:49:48 +0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -5,12 +5,12 @@ import { createClient } from "@supabase/supabase-js";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 dotenv.config();
 
-const loader = new PDFLoader("pdfs/doc3.pdf");
-const docs = await loader.load();
+//const loader = new PDFLoader("pdfs/doc3.pdf");
+//const docs = await loader.load();
 
-export const docs_data = () => {
-    return docs;
-}
+//export const docs_data = () => {
+    //return docs;
+//}
 
 const privateKey = process.env.SUPABASE_PRIVATE_KEY;
 if (!privateKey) throw new Error(`Expected env var SUPABASE_PRIVATE_KEY`);
