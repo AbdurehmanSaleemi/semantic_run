eae14acdc9fd286faa0854484d68de0c9656226b
Abdurehman Saleemi <76134547+AbdurehmanSaleemi@users.noreply.github.com>
2023-05-25 21:51:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { runPine } from "./run.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000
 
 //console.log(output);
 //const prompt = await runPine("Give me summary of the document");
